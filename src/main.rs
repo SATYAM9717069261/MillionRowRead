1865b876eb2f75e0c209f67b2f48e8911249ab05
Satyam Mandal <satyam.mandal.77@gmail.com>
2025-12-10 06:48:31 +0530

    use mmap and modify some code , also handle last line line.empty() {break;} reduce from 5s to 2.5s

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,45 @@
 use std::{
     collections::{BTreeMap, HashMap},
     fs::File,
-    io::{BufRead, BufReader},
+    os::fd::AsRawFd,
 };
 
 fn main() {
     let file = File::open("inp.txt").unwrap(); // @return Result<>
-    let buf_reader = BufReader::new(file);
-    let mut stats = HashMap::<String, (f64, f64, usize, f64)>::new(); // min, sum, count, max
-    for line in buf_reader.lines() {
-        match line {
-            Ok(data) => {
-                if let Some((name, tmp_)) = data.split_once(';') {
-                    let tmp: f64 = tmp_.parse().expect("");
-                    let ptr = match stats.get_mut(name) {
-                        Some(data) => data,
-                        None => {
-                            stats
-                                .entry(name.to_string())
-                                .or_insert((f64::MAX, 0.0, 0, f64::MIN))
-                        }
-                    };
-                    ptr.0 = ptr.0.min(tmp);
-                    ptr.1 += tmp;
-                    ptr.2 += 1;
-                    ptr.3 = ptr.3.max(tmp);
-                } else {
-                    println!("Split Error: {:?} ", data);
-                }
-            }
-            Err(err) => {
-                print!("Issue in Buffer Reading {err}");
-            }
+    let map = mmap(&file);
+    let mut stats = HashMap::<Vec<u8>, (f64, f64, usize, f64)>::new(); // min, sum, count, max
+    for line in map.split(|data| *data == b'\n') {
+        if line.is_empty() {
+            break;
         }
+        let mut fields = line.rsplitn(2, |c| *c == b';');
+        let _tmp = fields.next().unwrap();
+        let name = fields.next().unwrap();
+
+        let tmp: f64 = unsafe { std::str::from_utf8_unchecked(_tmp) }
+            .parse()
+            .unwrap();
+        let ptr = match stats.get_mut(name) {
+            Some(data) => data,
+            None => stats
+                .entry(name.to_vec())
+                .or_insert((f64::MAX, 0.0, 0, f64::MIN)),
+        };
+        ptr.0 = ptr.0.min(tmp);
+        ptr.1 += tmp;
+        ptr.2 += 1;
+        ptr.3 = ptr.3.max(tmp);
     }
     display(&stats);
 }
-fn display(stats: &HashMap<String, (f64, f64, usize, f64)>) {
+fn display(stats: &HashMap<Vec<u8>, (f64, f64, usize, f64)>) {
     let stats = BTreeMap::from_iter(stats);
     let mut iter = stats.into_iter().peekable();
     print!("{{");
     while let Some((station, (min_tmp, sum, count, max_tmp))) = iter.next() {
         print!(
-            "{station}={min_tmp:.1}/{:.1}/{max_tmp:.1}",
+            "{:?}={min_tmp:.1}/{:.1}/{max_tmp:.1}",
+            station,
             sum / (*count as f64)
         );
         if iter.peek().is_some() {
@@ -50,4 +47,26 @@ fn display(stats: &HashMap<String, (f64, f64, usize, f64)>) {
         }
     }
     print!("}}");
+}
+
+fn mmap(f: &File) -> &'_ [u8] {
+    let len = f.metadata().unwrap().len();
+    unsafe {
+        let ptr = libc::mmap(
+            std::ptr::null_mut(),
+            len as libc::size_t,
+            libc::PROT_READ,
+            libc::MAP_SHARED,
+            f.as_raw_fd(),
+            0,
+        );
+        if ptr == libc::MAP_FAILED {
+            panic!("{:?}", std::io::Error::last_os_error())
+        } else {
+            if libc::madvise(ptr, len as libc::size_t, libc::MADV_SEQUENTIAL) != 0 {
+                panic!("{:?}", std::io::Error::last_os_error())
+            }
+            return std::slice::from_raw_parts(ptr as *const u8, len as usize);
+        }
+    }
 }
